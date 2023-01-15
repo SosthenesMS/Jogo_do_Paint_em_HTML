f931ba623210f4fa24205c9cdc90126614d5ba98
SosthenesMS <sosthenesms@yahoo.com.br>
2023-01-15 18:24:23 -0300

    Update: input para selecao de cores

--- DesenhoLivre.js
+++ DesenhoLivre.js
@@ -1,8 +1,10 @@
 var tela = document.querySelector('canvas');
+var selecaoCor = document.querySelector('input');
 var pincel = tela.getContext('2d');
 var xEvento, yEvento, contadorCor = 0,raio = 20;
 var cores = ["blue","red","green","white"];
 var desenha = false;
+var iCount=0;
 
 pincel.fillStyle = "white";
 pincel.fillStroke = "black";
@@ -12,42 +14,73 @@ pincel.fill();
 function desenhaCirculo(evento){
 
     if(desenha){
+        if(iCount == 1){
+            xEvento = evento.pageX - tela.offsetLeft;
+            yEvento = evento.pageY - tela.offsetTop;
 
-        xEvento = evento.pageX - tela.offsetLeft;
-        yEvento = evento.pageY - tela.offsetTop;
+            pincel.fillStyle = 'White';
+            pincel.beginPath();
+            pincel.arc(xEvento, yEvento, raio, 0, (2*Math.PI));
+            pincel.fill();
+            console.log(xEvento + " , " + yEvento);
+            console.log(evento.shiftkey);
+            console.log(evento.onmousedown);
 
-        pincel.fillStyle = cores[contadorCor];
-        pincel.beginPath();
-        pincel.arc(xEvento, yEvento, raio, 0, (2*Math.PI));
-        pincel.fill();
-        console.log(xEvento + " , " + yEvento);
-        console.log(evento.shiftkey);
-        console.log(evento.onmousedown);
+        }else if(iCount == 0){
+            xEvento = evento.pageX - tela.offsetLeft;
+            yEvento = evento.pageY - tela.offsetTop;
+
+            pincel.fillStyle = selecaoCor.value;
+            pincel.beginPath();
+            pincel.arc(xEvento, yEvento, raio, 0, (2*Math.PI));
+            pincel.fill();
+            console.log(xEvento + " , " + yEvento);
+            console.log(evento.shiftkey);
+            console.log(evento.onmousedown);
+        }
+        
     }
     
 }
 
 tela.onmousemove = desenhaCirculo;
 
+//ORIGINAL
+// function mudaCor(){
+//     contadorCor++;
+
+//     if(contadorCor >= cores.length){
+//         contadorCor = 0;
+//         alert('Você mudou de cor para ' + "'" + cores[contadorCor] + "'!");
+        
+//     } else if(contadorCor == 3){
+//         alert("Você mudou para Borracha!");
+        
+
+//     } else {
+//         alert('Você mudou de cor para ' + "'" + cores[contadorCor] + "'!");
+        
+//    }
+
+//     return false;
+
+// }
+
 function mudaCor(){
-    contadorCor++;
 
-    if(contadorCor >= cores.length){
-        contadorCor = 0;
-        alert('Você mudou de cor para ' + "'" + cores[contadorCor] + "'!");
-        
-    } else if(contadorCor == 3){
-        alert("Você mudou para Borracha!");
-        
+    iCount++;
 
-    } else {
-        alert('Você mudou de cor para ' + "'" + cores[contadorCor] + "'!");
-        
-   }
+    if(iCount == 1){
+        alert("Você mudou para borracha!");
+    }else if(iCount > 1){
+        iCount = 0;
+        alert("Você mudou para o pincel, por favor escolha uma cor!");
+    }
 
     return false;
 
 }
+
 
 tela.oncontextmenu = mudaCor;
 
